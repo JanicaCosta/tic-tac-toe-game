3f8cd8501a3eae158a411224bb9be1487e58e619
JanicaCosta <joaocarocco@gmail.com>
2023-02-27 22:58:39 +0000

    add functions to java and winer combinations

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,17 @@ const cells = document.querySelectorAll(".cell");
 
 let checkTurn = true ;
 
+let winerCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
 /** defining players */
 
 let player_X = "X";
@@ -22,4 +33,16 @@ function play(id) {
     turn = checkTurn ? player_X : player_O;
     cell.textContent = turn;
     checkTurn = !checkTurn;
+}
+
+function checkWiner() {
+
+}
+
+function checkDraw(){
+
+}
+
+function gameCompleted(){
+
 }
